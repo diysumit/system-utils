72f0f1d0a223b6a1c25cd6b4af55572b98f07924
Sumit <diysumit@gmail.com>
2022-08-27 02:47:41 +0530

    small fix

--- rename.sh
+++ rename.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #* after mv under quotes give the name of the files to be renamed
-for i in *.png; do mv $i "${i}".png; done
+for i in {1..52}; do mv "ss (${i}).png" "${i}".png; done
 #* with few modifications can also be used to add new sequences in the file or do something repeatedly
