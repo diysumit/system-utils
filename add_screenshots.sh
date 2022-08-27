f7737b0b7dbe936e2a7336af36d6f55140eeccbf
Sumit <diysumit@gmail.com>
2022-08-27 08:34:58 +0530

    Small fix

--- add_screenshots.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-#* adding screenshots to README.md file
-for i in $(seq 1 193); do echo "![](./Screenshots/$i.png)" >> README.md; done
--- rename.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#* after mv under quotes give the name of the files to be renamed
-for i in {1..52}; do mv "ss (${i}).png" "${i}".png; done
-#* with few modifications can also be used to add new sequences in the file or do something repeatedly
